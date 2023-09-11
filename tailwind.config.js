59508106c02a800d5ccc16a02d54ee365ac1ed0e
Lennard Langenbruch <lennard.langenbruch@gmail.com>
2023-09-11 19:59:49 +0200

    multiple ui fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,11 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     fontSize: {
-      sm: ['2vh'],
-      md: ['2.5vh'],
-      lg: ['5vh']
+      sm: ['3vh'],
+      md: ['4vh'],
+      lg: ['5vh'],
+      xl: ['6vh'],
+      mobile: ['60px']
     }
   },
   plugins: [],
